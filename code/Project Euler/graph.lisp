0188864d578ad358a73200830d330df4248512ef
Sean Starkey <sean@seanstarkey.com>
2010-12-06 23:27:27 +0000

    Update to Graph library
    git-svn-id: file:///data1/svn/euler@35 4998059b-2d86-4de2-b56f-1eca0c2c1d03

--- code/Project Euler/graph.lisp
+++ code/Project Euler/graph.lisp
@@ -7,11 +7,17 @@
 (defparameter graph-test-data '((a
                                  ((b 2) (c 3)))
                                 (b
-                                 ((d 3)))
+                                 ((d 3) (e 1)))
                                 (c
                                  ((d 3)))
                                 (d
-                                 ((nil)))))
+                                 ((e 3) (f 2)))
+                                (e
+                                 ((g 3)))
+                                (f
+                                 ((g 3)))
+                                (g ((nil)))))
+(defun test () (shortest-path 'a 'g graph-test-data))
 
 (defun nodes (graph)
   "returns a list of all the nodes"
@@ -24,15 +30,55 @@
   "Returns a node given a graph"
   (find node graph :key #'car))
 
+(defun vertices (node graph)
+  (cadr (node node graph)))
+
+;;
+;; See wikipedia article for pseudocode
+;; http://en.wikipedia.org/wiki/Dijkstra's_algorithm
+;;
 (defun shortest-path (start end graph)
   "Returns the shortest path in a graph using Dijkstra's algorithm"
   (let ((dist (make-hash-table))
         (previous (make-hash-table))
         (q (make-hash-table)))
+    ;; Initialize variables
     (dolist (node (nodes graph))
       (setf (gethash node dist) most-positive-fixnum)
       (setf (gethash node previous) nil)
       (setf (gethash node q) t))
     (setf (gethash start dist) 0)
-    (print (hash-table-count dist)))
-  nil)
+    (shortest-path-loop start end graph dist previous q)
+    (let ((path)
+          (u end))
+      (loop while (not (eql u start)) do
+            (push u path)
+            (setf u (gethash u previous)))
+      (push start path)
+      path)))
+
+(defun shortest-path-loop (start end graph dist previous q)
+  (loop while (/= 0 (hash-table-count q)) do
+        (let ((u (get-smallest-dist q dist)))
+          (if (eq (gethash u dist) most-positive-fixnum)
+              (return-from shortest-path-loop))
+          (if (eq u end)
+              (return-from shortest-path-loop))
+          (remhash u q)
+          (dolist (v (vertices u graph))
+            (let ((alt (+ (gethash u dist) (cadr v))))
+              (if (< alt (gethash (car v) dist))
+                  (setf (gethash (car v) dist) alt
+                        (gethash (car v) previous) u)))))))
+
+;;;
+;;; Gets the smallest computed distance of nodes in q
+;;;
+(defun get-smallest-dist (q dist)
+  (let ((shortest-dist most-positive-fixnum)
+        (u nil))
+    (maphash #'(lambda (k v)
+                 (if (< (gethash k dist) shortest-dist)
+                     (setf shortest-dist (gethash k dist)
+                           u k))) q)
+    u))
