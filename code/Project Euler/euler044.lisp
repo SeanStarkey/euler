509ef31cf7d98dd7979846b8cf95f3ce3eab1ae8
Sean Starkey <sean@seanstarkey.com>
2012-03-18 13:54:42 +0000

    Project euler problem 44
    git-svn-id: file:///data1/svn/euler@57 4998059b-2d86-4de2-b56f-1eca0c2c1d03

---
+++ code/Project Euler/euler044.lisp
@@ -0,0 +1,45 @@
+;;;;
+;;;; Project euler problem 44
+;;;;
+
+(defparameter *size* 10000)
+(defparameter *pentagonal-hash* (make-hash-table))
+
+(defun euler044 ()
+  (let ((pentagonal-array (init-pentagonal-array *size*)))
+
+    (do ((x 1 (1+ x)))
+        ((> x *size*))
+      (do ((y 1 (1+ y)))
+          ((>= y x))
+        (let* ((pent-x (pentagonal x))
+               (pent-y (pentagonal y))
+               (sum (+ pent-x pent-y))
+               (diff (- pent-x pent-y)))
+;          (if (and (member-array sum pentagonal-array)
+;                   (member-array diff pentagonal-array))
+          (if (and (pentagonalp sum)
+                   (pentagonalp diff))
+              (return-from euler044 diff)))))))
+
+(defun init-pentagonal-array (size)
+  (let ((return-array (make-array (1+ size))))
+    (do ((index 1 (1+ index)))
+        ((> index size) return-array)
+      (let ((pent (pentagonal index)))
+        (setf (gethash pent *pentagonal-hash*) t)
+        (setf (aref return-array index) pent)))))
+
+(defun pentagonal (n)
+  (/ (* n (- (* 3 n) 1)) 2))
+
+(defun pentagonalp (n)
+  (nth-value 0 (gethash n *pentagonal-hash*)))
+
+(defun member-array (n array)
+  (do ((index 1 (1+ index)))
+      (nil)
+      (if (= (aref array index) n)
+          (return t)
+          (if (> (aref array index) n)
+              (return nil)))))
