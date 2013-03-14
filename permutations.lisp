32a0a039733962a038d97289d2e34dfcbf815b82
Sean Starkey <sean@seanstarkey.com>
2013-03-13 19:30:11 -0600

    Problem 62

--- permutations.lisp
+++ permutations.lisp
@@ -13,6 +13,15 @@
 (defun permutations-number (number)
   (mapcar #'list-to-number (permutations (number-to-list number))))
 
+(defun same-bag-p (bag1 bag2 &key (test #'eql))
+  (let ((table (make-hash-table :test test)))
+    (loop for key in bag1 do (incf (gethash key table 0)))
+    (loop for key in bag2 do (decf (gethash key table 0)))
+    (loop for val being each hash-value of table always (= val 0))))
+
+(defun permutation? (a b)
+  (same-bag-p a b))
+
 (defun number-to-list (number)
   (if (zerop number)
       nil
@@ -28,13 +37,13 @@
       0
       (+ (car lst) (* 10 (list-to-number-helper (cdr lst))))))
 
-(defun ! (n)
+(defun fact (n)
   (if (= n 0)
       1
-      (* n (! (- n 1)))))
+      (* n (fact (- n 1)))))
 
 (defun c (n r)
-  (/ (! n) (* (! r) (! (- n r)))))
+  (/ (fact n) (* (fact r) (fact (- n r)))))
 
 (defun p (n r)
-  (/ (! n) (! (- n r))))
+  (/ (fact n) (fact (- n r))))
